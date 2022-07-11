15ac8d8ed271cece5c67e0f9090713f09958ddc8
Adorjan Cizmar <adorjan.cizmar@gmail.com>
2022-07-11 09:14:40 +0200

    initial commit

---
+++ src/main/java/com/adorjanpraksa/MarsRoverImagesApiApplication.java
@@ -0,0 +1,13 @@
+package com.adorjanpraksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverImagesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverImagesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adorjanpraksa/MarsRoverImagesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adorjanpraksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverImagesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
